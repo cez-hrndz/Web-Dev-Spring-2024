a31734e3979dae0561d56dd1cfe2471519f3b2f8
cez-hrndz <114443333+cez-hrndz@users.noreply.github.com>
2024-04-25 17:23:50 -0500

    continue to work on react and the logic for how to count the days without hard coding

---
+++ main_calander_react/src/styles.js
@@ -0,0 +1,13 @@
+import styles from 'styles-components';
+
+export const Wrapper=styles.div
+
+width: '100%';
+height: '100vh';
+border: '1px solid'; 
+margin: '5px';
+;
+
+export const CalenderHead= styles.div
+width: '100%';
+
