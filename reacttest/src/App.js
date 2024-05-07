33ae172457b865059c00d4e946fae482d6fc0356
Msheph1 <max.r.shep@gmail.com>
2024-05-07 18:41:29 -0500

    react proj

---
+++ reacttest/src/App.js
@@ -0,0 +1,63 @@
+
+
+function App() {
+  return (
+    <div className="App">
+      <div className="header">Header</div>
+      <div className="filters">Filters</div>
+      <div className="cal">
+        <div className="row">
+          <div className="day" id="0"></div>
+          <div className="day" id="0"></div>
+          <div className="day" id="0"></div>
+          <div className="day" id="1"></div>
+          <div className="day" id="2"></div>
+          <div className="day" id="3"></div>
+          <div className="day" id="4"></div>
+        </div>
+        <div className="row">
+          <div className="day" id="5"></div>
+          <div className="day" id="6"></div>
+          <div className="day" id="7"></div>
+          <div className="day" id="8"></div>
+          <div className="day" id="9"></div>
+          <div className="day" id="10"></div>
+          <div className="day" id="11"></div>
+        </div>
+        <div className="row">
+          <div className="day" id="12"></div>
+          <div className="day" id="13"></div>
+          <div className="day" id="14"></div>
+          <div className="day" id="15"></div>
+          <div className="day" id="16"></div>
+          <div className="day" id="17"></div>
+          <div className="day" id="18"></div>
+        </div>
+        <div className="row">
+          <div className="day" id="19"></div>
+          <div className="day" id="20"></div>
+          <div className="day" id="21"></div>
+          <div className="day" id="22"></div>
+          <div className="day" id="23"></div>
+          <div className="day" id="24"></div>
+          <div className="day" id="25"></div>
+        </div>
+        <div className="row">
+          <div className="day" id="26"></div>
+          <div className="day" id="27"></div>
+          <div className="day" id="28"></div>
+          <div className="day" id="29"></div>
+          <div className="day" id="30"></div>
+          <div className="day" id="31"></div>
+          <div className="day" id="0"></div>
+        </div>
+        
+       
+      </div>
+      <div className="newEvent">Create Event</div>
+      <div className="footer">Footer</div>
+    </div>
+  );
+}
+
+export default App;
